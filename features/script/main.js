3c7df9e695a2a6a9ef8ee06affbd6343079c1986
Adrian Flores Pizarro <adfpizarro@gmail.com>
2021-04-14 13:08:14 -0600

    Refactor object constructor

--- features/script/main.js
+++ features/script/main.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+const Book = (title, author, pages, read) => {
   this.title = title;
   this.author = author;
   this.pages = pages;
